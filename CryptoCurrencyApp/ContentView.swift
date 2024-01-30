8903d90d028897311cc03444d97bee4bac732512
pinaralper <ppinaralper@gmail.com>
2024-01-30 12:50:37 +0300

    Initial Commit

---
+++ CryptoCurrencyApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptoCurrencyApp
+//
+//  Created by PINAR KALKAN on 30.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoCurrencyApp/CryptoCurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyAppApp.swift
+//  CryptoCurrencyApp
+//
+//  Created by PINAR KALKAN on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
